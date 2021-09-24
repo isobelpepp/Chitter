ce44e867411fcbe334e60c80bed57dd660b832d9
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-09-24 13:50:46 +0100

    Adds created_at to a peep upon creation

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,24 +2,25 @@ require_relative 'database_connection'
 
 class Peep
 
-  attr_reader :peep_id, :content
+  attr_reader :peep_id, :content, :created_at
 
-  def initialize(peep_id:, content:)
+  def initialize(peep_id:, content:, created_at:)
     @peep_id = peep_id
     @content = content
+    @created_at = created_at
   end
 
   def self.create(content:)
-    result = DatabaseConnection.query("INSERT INTO peeps (content) 
+    result = connection.exec("INSERT INTO peeps (content) 
                                       VALUES ('#{content}')
-                                      RETURNING peep_id, content;")
-    Peep.new(peep_id: result[0]['peep_id'], content: content)
+                                      RETURNING peep_id, content, to_char(created_at, 'HH24:MI, DD/MM/YYYY');")
+    Peep.new(peep_id: result[0]['peep_id'], content: content, created_at: result[0]['to_char'])
   end
 
   def self.all
-    result = DatabaseConnection.query("SELECT * FROM peeps;")
+    result = connection.exec("SELECT peep_id, content, to_char(created_at, 'HH24:MI, DD/MM/YYYY') FROM peeps;")
     result.map do |peep|
-      Peep.new(peep_id: peep['peep_id'], content: peep['content'])
+      Peep.new(peep_id: peep['peep_id'], content: peep['content'], created_at: peep['to_char'])
     end
   end
 end
--- spec/models/peep_spec.rb
+++ spec/models/peep_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../../lib/peep'
 require_relative '../helpers/persisted_data'
+require 'timecop'
 
 describe Peep do
   describe '.create' do
@@ -10,7 +11,15 @@ describe Peep do
       expect(peep.content).to eq('A peep')
       expect(peep.peep_id).to eq persisted_data[0]['peep_id']
     end
+    it 'records the time a peep is created' do
+      time = Timecop.freeze
+      peep = Peep.create(content: 'A peep')
+      expect(peep.created_at).to eq(time.strftime('%H:%M, %d/%m/%Y'))
+    end
   end
+
+  # 2021-09-24 13:19:49.067616 +0100
+  # Time.now.strftime("%d/%m/%Y %H:%M")
 
   describe '.all' do
     it 'returns all the peeps in the database' do
